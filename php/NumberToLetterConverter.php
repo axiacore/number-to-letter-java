7bcf2b8a3e5a6478a4bf84e56855ca98650020d2
diegovargas <silvernn357@gmail.com>
2017-11-24 17:17:03 -0600

    Add files via upload

---
+++ php/NumberToLetterConverter.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * Clase que implementa un conversor de números a letras. 
+ * @author AxiaCore S.A.S
+ *
+ */
+
+class NumberToLetterConverter {
+  private $UNIDADES = array(
+        '',
+        'UN ',
+        'DOS ',
+        'TRES ',
+        'CUATRO ',
+        'CINCO ',
+        'SEIS ',
+        'SIETE ',
+        'OCHO ',
+        'NUEVE ',
+        'DIEZ ',
+        'ONCE ',
+        'DOCE ',
+        'TRECE ',
+        'CATORCE ',
+        'QUINCE ',
+        'DIECISEIS ',
+        'DIECISIETE ',
+        'DIECIOCHO ',
+        'DIECINUEVE ',
+        'VEINTE '
+  );
+
+  private $DECENAS = array(
+        'VEINTI',
+        'TREINTA ',
+        'CUARENTA ',
+        'CINCUENTA ',
+        'SESENTA ',
+        'SETENTA ',
+        'OCHENTA ',
+        'NOVENTA ',
+        'CIEN '
+  );
+
+  private $CENTENAS = array(
+        'CIENTO ',
+        'DOSCIENTOS ',
+        'TRESCIENTOS ',
+        'CUATROCIENTOS ',
+        'QUINIENTOS ',
+        'SEISCIENTOS ',
+        'SETECIENTOS ',
+        'OCHOCIENTOS ',
+        'NOVECIENTOS '
+  );
+
+  private $MONEDAS = array(
+    array('country' => 'Colombia', 'currency' => 'COP', 'singular' => 'PESO COLOMBIANO', 'plural' => 'PESOS COLOMBIANOS', 'symbol', '$', 'decimal' => 'CENTAVOS'),
+    array('country' => 'Estados Unidos', 'currency' => 'USD', 'singular' => 'DÓLAR', 'plural' => 'DÓLARES', 'symbol', 'US$', 'decimal' => 'CENTAVOS'),
+    array('country' => 'El Salvador', 'currency' => 'USD', 'singular' => 'DÓLAR', 'plural' => 'DÓLARES', 'symbol', 'US$', 'decimal' => 'CENTAVOS'),
+    array('country' => 'Europa', 'currency' => 'EUR', 'singular' => 'EURO', 'plural' => 'EUROS', 'symbol', '€', 'decimal' => 'CENTAVOS'),
+    array('country' => 'México', 'currency' => 'MXN', 'singular' => 'PESO', 'plural' => 'PESOS', 'symbol', '$', 'decimal' => 'CENTAVOS'),
+    array('country' => 'Perú', 'currency' => 'PEN', 'singular' => 'NUEVO SOL', 'plural' => 'NUEVOS SOLES', 'symbol', 'S/', 'decimal' => 'CENTAVOS'),
+    array('country' => 'Reino Unido', 'currency' => 'GBP', 'singular' => 'LIBRA', 'plural' => 'LIBRAS', 'symbol', '£', 'decimal' => 'CENTAVOS'),
+    array('country' => 'Argentina', 'currency' => 'ARS', 'singular' => 'PESO', 'plural' => 'PESOS', 'symbol', '$', 'decimal' => 'CENTAVOS')
+  );
+
+    private $separator = ',';
+    private $decimal_mark = '.';
+    private $glue = ' CON ';
+
+    /**
+     * Evalua si el número contiene separadores o decimales
+     * formatea y ejecuta la función conversora
+     * @param $number número a convertir
+     * @param $miMoneda clave de la moneda
+     * @return string completo
+     */
+    public function to_word($number, $miMoneda = null) {
+        if (strpos($number, $this->decimal_mark) === FALSE) {
+          $convertedNumber = array(
+            $this->convertNumber($number, $miMoneda, 'entero')
+          );
+        } else {
+          $number = explode($this->decimal_mark, str_replace($this->separator, '', trim($number)));
+          if(strlen($number[1]) == 1)
+            $number[1] *= 10;  
+          $convertedNumber = array(
+            $this->convertNumber($number[0], $miMoneda, 'entero'),
+            $this->convertNumber($number[1], $miMoneda, 'decimal'),
+          );
+        }
+        return implode($this->glue, array_filter($convertedNumber));
+    }
+
+    /**
+     * Convierte número a letras
+     * @param $number
+     * @param $miMoneda
+     * @param $type tipo de dígito (entero/decimal)
+     * @return $converted string convertido
+     */
+    private function convertNumber($number, $miMoneda = null, $type) {   
+        
+        $converted = '';
+        if ($miMoneda !== null) {
+            try {
+                
+                $moneda = array_filter($this->MONEDAS, function($m) use ($miMoneda) {
+                    return ($m['currency'] == $miMoneda);
+                });
+
+                $moneda = array_values($moneda);
+
+                if (count($moneda) <= 0) {
+                    throw new Exception("Tipo de moneda inválido");
+                    return;
+                }
+                if($type == 'entero')
+                    ($number < 2 ? $moneda = $moneda[0]['singular'] : $moneda = $moneda[0]['plural']);
+                if($type == 'decimal')
+                ($number < 2 ? $moneda = $moneda[0]['decimal'] : $moneda = $moneda[0]['decimal']);
+            } catch (Exception $e) {
+                echo $e->getMessage();
+                return;
+            }
+        }else{
+            $moneda = '';
+        }
+
+        if (($number < 0) || ($number > 999999999)) {
+            return false;
+        }
+
+        $numberStr = (string) $number;
+        $numberStrFill = str_pad($numberStr, 9, '0', STR_PAD_LEFT);
+        $millones = substr($numberStrFill, 0, 3);
+        $miles = substr($numberStrFill, 3, 3);
+        $cientos = substr($numberStrFill, 6);
+        
+        if (intval($millones) > 0) {
+            if ($millones == '001') {
+                $converted .= 'UN MILLON ';
+            } else if (intval($millones) > 0) {
+                $converted .= sprintf('%sMILLONES ', $this->convertGroup($millones));
+            }
+        }
+        
+        if (intval($miles) > 0) {
+            if ($miles == '001') {
+                $converted .= 'MIL ';
+            } else if (intval($miles) > 0) {
+                $converted .= sprintf('%sMIL ', $this->convertGroup($miles));
+            }
+        }
+
+        if (intval($cientos) > 0) {
+            if ($cientos == '001') {
+                $converted .= 'UN ';
+            } else if (intval($cientos) > 0) {
+                $converted .= sprintf('%s ', $this->convertGroup($cientos));
+            }
+        }
+
+        $converted .= $moneda;
+
+        return $converted;
+    }
+
+    /**
+     * Define el tipo de representación decimal (centenas/millares/millones)
+     * @param $n
+     * @return $output
+     */
+    private function convertGroup($n) {
+
+        $output = '';
+
+        if ($n == '100') {
+            $output = "CIEN ";
+        } else if ($n[0] !== '0') {
+            $output = $this->CENTENAS[$n[0] - 1];   
+        }
+
+        $k = intval(substr($n,1));
+
+        if ($k <= 20) {
+            $output .= $this->UNIDADES[$k];
+        } else {
+            if(($k > 30) && ($n[2] !== '0')) {
+                $output .= sprintf('%sY %s', $this->DECENAS[intval($n[1]) - 2], $this->UNIDADES[intval($n[2])]);
+            } else {
+                $output .= sprintf('%s%s', $this->DECENAS[intval($n[1]) - 2], $this->UNIDADES[intval($n[2])]);
+            }
+        }
+
+        return $output;
+    }
+}
