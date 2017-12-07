48a7db06cb3b57b2046f08cb9ce889d7469cf788
Carlos Sifuentes <sifuentescarlos226@gmail.com>
2017-12-06 19:29:19 -0600

    Modificado para mostrar CENTAVOS y CERO (cuando es el caso)

--- php/NumberToLetterConverter.class.php
+++ php/NumberToLetterConverter.class.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * Clase que implementa un conversor de números a letras. 
+ * Clase que implementa un conversor de números a letras.
  * @author AxiaCore S.A.S
- *
  */
-
-class NumberToLetterConverter {
-  private $UNIDADES = array(
+class NumberToLetterConverter
+{
+    private $UNIDADES = [
         '',
         'UN ',
         'DOS ',
@@ -28,9 +27,8 @@ class NumberToLetterConverter {
         'DIECIOCHO ',
         'DIECINUEVE ',
         'VEINTE '
-  );
-
-  private $DECENAS = array(
+    ];
+    private $DECENAS = [
         'VEINTI',
         'TREINTA ',
         'CUARENTA ',
@@ -40,9 +38,8 @@ class NumberToLetterConverter {
         'OCHENTA ',
         'NOVENTA ',
         'CIEN '
-  );
-
-  private $CENTENAS = array(
+    ];
+    private $CENTENAS = [
         'CIENTO ',
         'DOSCIENTOS ',
         'TRESCIENTOS ',
@@ -52,22 +49,115 @@ class NumberToLetterConverter {
         'SETECIENTOS ',
         'OCHOCIENTOS ',
         'NOVECIENTOS '
-  );
+    ];
+    private $MONEDAS = [
+        [
+            'country' => 'Colombia',
+            'currency' => 'COP',
+            'singular' => 'PESO COLOMBIANO',
+            'plural' => 'PESOS COLOMBIANOS',
+            'decimal' => [
+                'singular' => 'CENTAVO',
+                'plural' => 'CENTAVOS'
+            ],
+            'symbol',
+            '$'
+        ],
+        [
+            'country' => 'Estados Unidos',
+            'currency' => 'USD',
+            'singular' => 'DÓLAR',
+            'plural' => 'DÓLARES',
+            'decimal' => [
+                'singular' => 'CENTAVO',
+                'plural' => 'CENTAVOS'
+            ],
+            'symbol',
+            'US$'
+        ],
+        [
+            'country' => 'El Salvador',
+            'currency' => 'USD',
+            'singular' => 'DÓLAR',
+            'plural' => 'DÓLARES',
+            'decimal' => [
+                'singular' => 'CENTAVO',
+                'plural' => 'CENTAVOS'
+            ],
+            'symbol',
+            'US$'
+        ],
+        [
+            'country' => 'Europa',
+            'currency' => 'EUR',
+            'singular' => 'EURO',
+            'plural' => 'EUROS',
+            'decimal' => [
+                'singular' => 'CENTAVO',
+                'plural' => 'CENTAVOS'
+            ],
+            'symbol',
+            '€'
+        ],
+        [
+            'country' => 'México',
+            'currency' => 'MXN',
+            'singular' => 'PESO MEXICANO',
+            'plural' => 'PESOS MEXICANOS',
+            'decimal' => [
+                'singular' => 'CENTAVO',
+                'plural' => 'CENTAVOS'
+            ],
+            'symbol',
+            '$'
+        ],
+        [
+            'country' => 'Perú',
+            'currency' => 'PEN',
+            'singular' => 'NUEVO SOL',
+            'plural' => 'NUEVOS SOLES',
+            'decimal' => [
+                'singular' => 'CENTAVO',
+                'plural' => 'CENTAVOS'
+            ],
+            'symbol',
+            'S/'
+        ],
+        [
+            'country' => 'Reino Unido',
+            'currency' => 'GBP',
+            'singular' => 'LIBRA',
+            'plural' => 'LIBRAS',
+            'decimal' => [
+                'singular' => 'CENTAVO',
+                'plural' => 'CENTAVOS'
+            ],
+            'symbol',
+            '£'
+        ],
+        [
+            'country' => 'Argentina',
+            'currency' => 'ARS',
+            'singular' => 'PESO',
+            'plural' => 'PESOS',
+            'decimal' => [
+                'singular' => 'CENTAVO',
+                'plural' => 'CENTAVOS'
+            ],
+            'symbol',
+            '$'
+        ]
+    ];
+    private $separator;
+    private $decimal_mark;
+    private $glue;
 
-  private $MONEDAS = array(
-    array('country' => 'Colombia', 'currency' => 'COP', 'singular' => 'PESO COLOMBIANO', 'plural' => 'PESOS COLOMBIANOS', 'symbol', '$'),
-    array('country' => 'Estados Unidos', 'currency' => 'USD', 'singular' => 'DÓLAR', 'plural' => 'DÓLARES', 'symbol', 'US$'),
-    array('country' => 'El Salvador', 'currency' => 'USD', 'singular' => 'DÓLAR', 'plural' => 'DÓLARES', 'symbol', 'US$'),
-    array('country' => 'Europa', 'currency' => 'EUR', 'singular' => 'EURO', 'plural' => 'EUROS', 'symbol', '€'),
-    array('country' => 'México', 'currency' => 'MXN', 'singular' => 'PESO MEXICANO', 'plural' => 'PESOS MEXICANOS', 'symbol', '$'),
-    array('country' => 'Perú', 'currency' => 'PEN', 'singular' => 'NUEVO SOL', 'plural' => 'NUEVOS SOLES', 'symbol', 'S/'),
-    array('country' => 'Reino Unido', 'currency' => 'GBP', 'singular' => 'LIBRA', 'plural' => 'LIBRAS', 'symbol', '£'),
-    array('country' => 'Argentina', 'currency' => 'ARS', 'singular' => 'PESO', 'plural' => 'PESOS', 'symbol', '$')
-  );
-
-    private $separator = '.';
-    private $decimal_mark = ',';
-    private $glue = ' CON ';
+    public function __construct($separator = ',', $decimalMark = '.', $glue = ' CON ')
+    {
+        $this->separator = $separator;
+        $this->decimal_mark = $decimalMark;
+        $this->glue = $glue;
+    }
 
     /**
      * Evalua si el número contiene separadores o decimales
@@ -76,18 +166,16 @@ class NumberToLetterConverter {
      * @param $miMoneda clave de la moneda
      * @return string completo
      */
-    public function to_word($number, $miMoneda = null) {
-        if (strpos($number, $this->decimal_mark) === FALSE) {
-          $convertedNumber = array(
-            $this->convertNumber($number, $miMoneda, 'entero')
-          );
+    public function toWord($number, $miMoneda = null)
+    {
+        if (strpos($number, $this->decimal_mark) === false) {
+            $convertedNumber = [ $this->convertNumber($number, $miMoneda, 'entero') ];
         } else {
-          $number = explode($this->decimal_mark, str_replace($this->separator, '', trim($number)));
-
-          $convertedNumber = array(
-            $this->convertNumber($number[0], $miMoneda, 'entero'),
-            $this->convertNumber($number[1], $miMoneda, 'decimal'),
-          );
+            $number = explode($this->decimal_mark, str_replace($this->separator, '', trim($number)));
+            $convertedNumber = [
+                $this->convertNumber($number[0], $miMoneda, 'entero'),
+                $this->convertNumber($number[1], $miMoneda, 'decimal'),
+            ];
         }
         return implode($this->glue, array_filter($convertedNumber));
     }
@@ -99,45 +187,59 @@ class NumberToLetterConverter {
      * @param $type tipo de dígito (entero/decimal)
      * @return $converted string convertido
      */
-    private function convertNumber($number, $miMoneda = null, $type) {   
-        
+    private function convertNumber($number, $miMoneda, $type)
+    {
         $converted = '';
         if ($miMoneda !== null) {
             try {
-                
-                $moneda = array_filter($this->MONEDAS, function($m) use ($miMoneda) {
+                $moneda = array_filter($this->MONEDAS, function ($m) use ($miMoneda) {
                     return ($m['currency'] == $miMoneda);
                 });
-
                 $moneda = array_values($moneda);
-
                 if (count($moneda) <= 0) {
                     throw new Exception("Tipo de moneda inválido");
                     return;
                 }
-                ($number < 2 ? $moneda = $moneda[0]['singular'] : $moneda = $moneda[0]['plural']);
+                
+                if ($number == 0) {
+                    if ($type == 'entero') {
+                        $moneda = $moneda[0]['plural'];
+                    } else {
+                        $moneda = $moneda[0]['decimal']['plural'];
+                    }
+                } elseif ($number < 2) {
+                    if ($type == 'entero') {
+                        $moneda = $moneda[0]['singular'];
+                    } else {
+                        $moneda = $moneda[0]['decimal']['singular'];
+                    }
+                } else {
+                    if ($type == 'entero') {
+                        $moneda = $moneda[0]['plural'];
+                    } else {
+                        $moneda = $moneda[0]['decimal']['plural'];
+                    }
+                }
+                // ($number < 2 ? $moneda = $moneda[0]['singular'] : $moneda = $moneda[0]['plural']);
             } catch (Exception $e) {
-                echo $e->getMessage();
+                echo $e;
                 return;
             }
-        }else{
+        } else {
             $moneda = '';
         }
-
         if (($number < 0) || ($number > 999999999)) {
             return false;
         }
-
         $numberStr = (string) $number;
         $numberStrFill = str_pad($numberStr, 9, '0', STR_PAD_LEFT);
         $millones = substr($numberStrFill, 0, 3);
         $miles = substr($numberStrFill, 3, 3);
         $cientos = substr($numberStrFill, 6);
-
         if (intval($millones) > 0) {
             if ($millones == '001') {
                 $converted .= 'UN MILLON ';
-            } else if (intval($millones) > 0) {
+            } else /*if (intval($millones) > 0) */{
                 $converted .= sprintf('%sMILLONES ', $this->convertGroup($millones));
             }
         }
@@ -145,21 +247,20 @@ class NumberToLetterConverter {
         if (intval($miles) > 0) {
             if ($miles == '001') {
                 $converted .= 'MIL ';
-            } else if (intval($miles) > 0) {
+            } else /*if (intval($miles) > 0) */{
                 $converted .= sprintf('%sMIL ', $this->convertGroup($miles));
             }
         }
-
         if (intval($cientos) > 0) {
             if ($cientos == '001') {
                 $converted .= 'UN ';
-            } else if (intval($cientos) > 0) {
+            } else /*if (intval($cientos) > 0) */{
                 $converted .= sprintf('%s ', $this->convertGroup($cientos));
             }
+        } elseif (intval($millones) < 1 && intval($miles) < 1) {
+            $converted .= 'CERO ';
         }
-
         $converted .= $moneda;
-
         return $converted;
     }
 
@@ -168,28 +269,24 @@ class NumberToLetterConverter {
      * @param $n
      * @return $output
      */
-    private function convertGroup($n) {
-
+    private function convertGroup($n)
+    {
         $output = '';
-
         if ($n == '100') {
             $output = "CIEN ";
-        } else if ($n[0] !== '0') {
-            $output = $this->CENTENAS[$n[0] - 1];   
+        } elseif ($n[0] !== '0') {
+            $output = $this->CENTENAS[$n[0] - 1];
         }
-
-        $k = intval(substr($n,1));
-
+        $k = intval(substr($n, 1));
         if ($k <= 20) {
             $output .= $this->UNIDADES[$k];
         } else {
-            if(($k > 30) && ($n[2] !== '0')) {
+            if (($k > 30) && ($n[2] !== '0')) {
                 $output .= sprintf('%sY %s', $this->DECENAS[intval($n[1]) - 2], $this->UNIDADES[intval($n[2])]);
             } else {
                 $output .= sprintf('%s%s', $this->DECENAS[intval($n[1]) - 2], $this->UNIDADES[intval($n[2])]);
             }
         }
-
         return $output;
     }
 }
